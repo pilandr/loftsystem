34863a6d38b7dad9bad1e36b03ad16fc8a94e49a
Andrei Piliushenko <pionbox@gmail.com>
2021-12-26 12:30:15 +0300

    add server loftsystem

---
+++ server/models/schemas/news.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const newsSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: [true, 'id required'],
+      unique: true
+    },
+    created_at: {
+      type: Date,
+    },
+    text: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    user: {
+      firstName: String,
+      id: String,
+      image: String,
+      middleName: String,
+      surName: String,
+      username: String
+    }
+    // refreshToken
+  }
+)
+
+const News = mongoose.model('news', newsSchema)
+
+module.exports = News
